851d6777f236dae883e7fb8a15919d578eae63ad
kahandboo <158439807+kahandboo@users.noreply.github.com>
2024-11-04 23:21:16 +0900

    Title: 작은 구슬의 이동, Time: 38ms, Memory: 15MB, Status: Passed - CodeTree

--- 241104/작은 구슬의 이동/small-marble-movement.py
+++ 241104/작은 구슬의 이동/small-marble-movement.py
@@ -1,27 +1,27 @@
-def in_range(x,y):
-    return x>=0 and y>=0 and x<n and y<n
+n, t = tuple(map(int, input().split()))
+x, y, c_dir = tuple(input().split())
 
-n, t = map(int, input().split())
-
-r,c,d = input().split()
-r = int(r)
-c = int(c)
-dy, dx = [1,0,0,-1],[0,1,-1,0]
-dict = {
-    'R':0,
-    'U':2,
-    'D':1,
-    'L':3
+mapper = {
+    'R': 0,
+    'D': 1,
+    'U': 2,
+    'L': 3
 }
+x, y, move_dir = int(x) - 1, int(y) - 1, mapper[c_dir]
 
-dirr = dict[d]
+dxs = [0, 1, -1,  0]
+dys = [1, 0,  0, -1]
+
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
 
 for _ in range(t):
-    nr, nc = r+dx[dirr], c+dy[dirr]
-    if not in_range(nr,nc):
-        dirr = 3 - dirr
+    nx, ny = x + dxs[move_dir], y + dys[move_dir]
+    if in_range(nx, ny):
+        x, y = nx, ny
     else:
-        r,c = nr, nc
+        move_dir = 3 - move_dir
 
-    
-print(r + 1,c + 1)
+print(x + 1, y + 1)
